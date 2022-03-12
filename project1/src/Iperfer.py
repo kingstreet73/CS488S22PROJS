ecce944d6cef28e7781800174848c45920bb53d0
kingstreet73 <eg07546n@pace.edu>
2022-03-11 23:49:48 -0500

    Turning this as barebones as possible - still errorssss

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -1,4 +1,3 @@
-from multiprocessing.sharedctypes import Value
 import socket
 import sys
 import time #https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
@@ -10,43 +9,41 @@ max_Port = 65535
 
 #Create and confirm 3 arguments we want passed in - hostname, port, time
 
-if len(sys.argv) !=3:
+if len(sys.argv) !=4:
     print("Error: missing or additional arguments")
     exit()
 
-server_Hostname = str(sys.argv[0])
-server_Port = int(sys.argv[1])
-server_Time = int(sys.argv[2])
+server_Hostname = str(sys.argv[1])
+server_Port = int(sys.argv[2])
+server_Time = int(sys.argv[3])
 server_Hostname = socket.gethostbyname(server_Hostname)
 
 
 #confirm server_Port is in range
-if((server_Port < min_Port) or (server_Port > max_Port)):
+if((server_Port < 1024) or (server_Port > 65535)):
     print("Error: port number must be in the range 1024 to 65535")
     exit()
 
 #-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-----------------------------------------------------
         
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+address = (server_Hostname, server_Port)
 init_Chunk = bytearray(1000)
 counter = 0
 #           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
 client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = (server_Hostname, server_Port)
 client_Socket.connect(address)
 #calc run time and bytes sent- close connection when done
-init_Time = time.time()
-while time.time() - init_Time < server_Time:
+init_Time = time.perf_counter()
+while init_Time - time.perf_counter() < server_Time:
     client_Socket.send(init_Chunk)
     counter += 1
-iperf_runtime = time.time() - init_Time
-client_Socket.close
+    continue
 #calc rate
-send_Rate = (counter * 8 / 1000) / iperf_runtime
+send_Rate = (counter * 8 / 1000) / server_Time
 
 print("sent={} KB rate={} Mbps".format(counter, send_Rate))
-exit()
+client_Socket.close()
    
 
 

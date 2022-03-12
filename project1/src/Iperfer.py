99200f913e9a978e1160cacba692fca1ac5a0b77
kingstreet73 <eg07546n@pace.edu>
2022-03-11 23:10:35 -0500

    modified original format to not be modular for autograder

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -9,34 +9,31 @@ min_Port = 1024
 max_Port = 65535
 
 #Create and confirm 3 arguments we want passed in - hostname, port, time
-args = sys.argv[1,2,3]
 
-server_Hostname = args[0]
-server_Port = args[1]
-server_Time = args[2]
-
-if len(args) !=3:
+if len(sys.argv) !=3:
         print("Error: missing or additional arguments")
         exit()
 
-if len(args) == 3:
+server_Hostname = sys.argv[0]
+server_Port = int(sys.argv[1])
+server_Time = int(sys.argv[2])
+
 #confirm server_Hostname is real
-    try:
-        socket.inet_aton(server_Hostname)
-    except OSError:
-        print("Error missing or additional arguments")
+try:
+    socket.inet_aton(server_Hostname)
+except OSError:
+    print("Error missing or additional arguments")
 
 #confirm server_Port is in range
-    try:
-        server_Port = int(server_Port)
-        if server_Port < min_Port or server_Port > max_Port:
-            print("Error: port number must be in the range {} to {}").format(min_Port, max_Port)
-            exit()
-            # confirm time is valid (float for import time)
-        server_Time = float(server_Time)
-    except ValueError:
-           print("Error: missing or additional arguments")            
-           exit()
+   
+server_Port = int(server_Port)
+if server_Port < min_Port or server_Port > max_Port:
+    print("Error: port number must be in the range {} to {}").format(min_Port, max_Port)
+    exit()
+    # confirm time is valid (float for import time)
+if server_Time != int(server_Time):
+    print("Error: missing or additional arguments")            
+    exit()
 
 #-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-------------------------------------------------------
 
@@ -46,24 +43,22 @@ if len(args) == 3:
         
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-    init_Chunk = bytes(chunk_Size)
-    counter = 0
-    #           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
-    client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_Socket.connect((server_Hostname,server_Port))
-    #calc run time and bytes sent- close connection when done
-    init_Time = time.time()
-    while time.time() - init_Time < server_Time:
-        client_Socket.sendall(init_Chunk)
-        counter += 1
-    iperf_runtime = time.time() - init_Time
-    client_Socket.close
-    #calc rate
-    send_Rate = (counter * 8 / 1000) / iperf_runtime
-
-    print("sent={} KB rate={} Mbps".format(counter, send_Rate))
-    exit()
+init_Chunk = bytes(chunk_Size)
+counter = 0
+#           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
+client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_Socket.connect((server_Hostname,server_Port))
+#calc run time and bytes sent- close connection when done
+init_Time = time.time()
+while time.time() - init_Time < server_Time:
+    client_Socket.sendall(init_Chunk)
+    counter += 1
+iperf_runtime = time.time() - init_Time
+client_Socket.close
+#calc rate
+send_Rate = (counter * 8 / 1000) / iperf_runtime
+print("sent={} KB rate={} Mbps".format(counter, send_Rate))
+exit()
    
 
 

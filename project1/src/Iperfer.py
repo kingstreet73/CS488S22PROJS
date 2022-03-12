dfe52c95e3bffe131214f48ea5d8ee4bf69d33e6
kingstreet73 <eg07546n@pace.edu>
2022-03-12 00:39:40 -0500

    attempt at server solution without modularity, if fails multiple time - go back to modular and add the main executor

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -2,43 +2,66 @@ import socket
 import sys
 import time #https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
 #VERSION 321091029301230129 COMPLETE, SIMPLIFIED REDO WITH NO SERVER, NO MODULES/FUNCTIONS, ONLY FOR AUTOGRADER
+#UPDATE: THIS VERSION WORKS FOR TASKS 1-3, BUT, REALIZED I TOOK OUT MY MAIN EXECUTOR FOR THE MODULAR DESIGN, SO PROBABLY GO BACK TO THAT IF THIS DOESNT WORK WITH SERVER
 #Create and confirm 3 arguments we want passed in - hostname, port, time
+if (len(sys.argv == 3)):
+    flag = str(sys.argv[1])
+    port = int(sys.argv[2])
+    if((port < 1024) or (port > 65535)):
+        print("Error: port number must be in the range 1024 to 65535")
+        exit()
+    init_Chunk = bytearray(1000)
+    #create server socket
+    server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #bind port with socket
+    server_Socket.bind(port)
+    #wait for 1 minimum client
+    server_Socket.listen(1)
+    #create connection socket
+    connection_socket, addr = server_Socket.accept()
+    while 1:
+        data = server_Socket.recv(init_Chunk)
 
-if (len(sys.argv) !=4):
+    connection_socket.close()
+    
+    
+
+
+elif (len(sys.argv) == 4): 
+
+    server_Hostname = str(sys.argv[1])
+    server_Port = int(sys.argv[2])
+    server_Time = int(sys.argv[3])
+    #confirm server_Port is in range
+    if((server_Port < 1024) or (server_Port > 65535)):
+        print("Error: port number must be in the range 1024 to 65535")
+        exit()
+
+    #-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-----------------------------------------------------
+
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    address = (server_Hostname, server_Port)
+    #           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
+    client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_Socket.connect(address)
+    init_Chunk = bytearray(1000)
+    counter = 0
+    #calc run time and bytes sent- close connection when done
+    timer = time.perf_counter()
+    while server_Time - timer > 0:
+        client_Socket.send(init_Chunk)
+        counter += 1
+        continue
+    #calc rate
+    send_Rate = (counter * 8 / 1000) / server_Time
+
+    print("sent={} KB rate={} Mbps".format(counter, send_Rate))
+    client_Socket.close()
+else:
     print("Error: missing or additional arguments")
-    exit()
-
-server_Hostname = str(sys.argv[1])
-server_Port = int(sys.argv[2])
-server_Time = int(sys.argv[3])
-#confirm server_Port is in range
-if((server_Port < 1024) or (server_Port > 65535)):
-    print("Error: port number must be in the range 1024 to 65535")
-    exit()
-
-#-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-----------------------------------------------------
-        
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-address = (server_Hostname, server_Port)
-#           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
-client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_Socket.connect(address)
-init_Chunk = bytearray(1000)
-counter = 0
-#calc run time and bytes sent- close connection when done
-timer = time.perf_counter()
-while server_Time - timer > 0:
-    client_Socket.send(init_Chunk)
-    counter += 1
-    continue
-#calc rate
-send_Rate = (counter * 8 / 1000) / server_Time
-
-print("sent={} KB rate={} Mbps".format(counter, send_Rate))
-client_Socket.close()
-   
+    exit()   
 
 
-   
+    
 
 

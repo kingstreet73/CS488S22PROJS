a66aadb15be0c52ac5bbabcf02e457f7d48dc90c
kingstreet73 <eg07546n@pace.edu>
2022-03-11 22:56:50 -0500

    testing without server to see autograder result

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -9,90 +9,44 @@ min_Port = 1024
 max_Port = 65535
 
 #Create and confirm 3 arguments we want passed in - hostname, port, time
-def client_args():
-    args = sys.argv[1,2,3]
+args = sys.argv[1,2,3]
 
-    server_Hostname = args[0]
-    server_Port = args[1]
-    server_Time = args[2]
+server_Hostname = args[0]
+server_Port = args[1]
+server_Time = args[2]
 
-    if len(args) !=3:
-        print("Error: missing or additional arguments")
-        exit()
-    if len(args) == 2:
-        server_args()
-        exit()
-    if len(args) == 3:
-    #confirm server_Hostname is real
-        try:
-            socket.inet_aton(server_Hostname)
-        except OSError:
-            print("Error missing or additional arguments")
-
-    #confirm server_Port is in range
-        try:
-            server_Port = int(server_Port)
-            if server_Port < min_Port or server_Port > max_Port:
-                print("Error: port number must be in the range {} to {}").format(min_Port, max_Port)
-                exit()
-            # confirm time is valid (float for import time)
-            server_Time = float(server_Time)
-        except ValueError:
-            print("Error: missing or additional arguments")
-            exit()
-
-    return server_Hostname, server_Port, server_Time
-#-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-------------------------------------------------------
-
-def server_args():
-    args = sys.argv[1,2]
-    
-    s_tag = args[0]
-    listen_Port = args[1]
-
-    #check if -s is set as tag
-    s_tag = str(s_tag)
-    if s_tag != "-s":
+if len(args) !=3:
         print("Error: missing or additional arguments")
         exit()
 
+if len(args) == 3:
+#confirm server_Hostname is real
     try:
-        listen_Port = int(listen_Port)
-        if listen_Port < min_Port or listen_Port > max_Port:
+        socket.inet_aton(server_Hostname)
+    except OSError:
+        print("Error missing or additional arguments")
+
+#confirm server_Port is in range
+    try:
+        server_Port = int(server_Port)
+        if server_Port < min_Port or server_Port > max_Port:
             print("Error: port number must be in the range {} to {}").format(min_Port, max_Port)
             exit()
+            # confirm time is valid (float for import time)
+        server_Time = float(server_Time)
     except ValueError:
-        print("Error: missing or additional arguments")
-        exit()
+           print("Error: missing or additional arguments")            
+           exit()
 
-    return listen_Port
+#-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-------------------------------------------------------
+
+
+
+
         
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-def server_Run(listen_port):
-    init_Chunk = bytes(chunk_Size)
-    
-    #create server socket
-    server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #bind port with socket
-    server_Socket.bind(listen_port)
-    #wait for 1 minimum client
-    server_Socket.listen(1)
-    #create connection socket
-    connection_socket, addr = server_Socket.accept()
-    while 1:
-        data = server_Socket.recv(init_Chunk)
-
-    connection_socket.close()
-    
-    return data
-
-    
-
-
-
-def client_Run(server_Hostname, server_Port, server_SetTime):
     init_Chunk = bytes(chunk_Size)
     counter = 0
     #           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)

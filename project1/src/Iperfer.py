bfa1a1685d81bb4d89a2a363c5b4f304092a4d2d
kingstreet73 <eg07546n@pace.edu>
2022-03-14 23:23:19 -0400

    server implementation test from deye and justin

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -4,30 +4,54 @@ import time #https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-
 #VERSION 321091029301230129 COMPLETE, SIMPLIFIED REDO WITH NO SERVER, NO MODULES/FUNCTIONS, ONLY FOR AUTOGRADER
 #UPDATE: THIS VERSION WORKS FOR TASKS 1-3, BUT, REALIZED I TOOK OUT MY MAIN EXECUTOR FOR THE MODULAR DESIGN, SO PROBABLY GO BACK TO THAT IF THIS DOESNT WORK WITH SERVER
 #Create and confirm 3 arguments we want passed in - hostname, port, time
+
+if (len(sys.argv) != 4) and (len(sys.argv) != 3):
+    print("Error: missing or additional arguments")
+    sys.exit()
+
+#server part
 if (len(sys.argv) == 3):
-    flag = str(sys.argv[1])
-    port = int(sys.argv[2])
-    if((port < 1024) or (port > 65535)):
-        print("Error: port number must be in the range 1024 to 65535")
-        exit()
-    init_Chunk = bytearray(1000)
-    #create server socket
-    server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #bind port with socket
-    server_Socket.bind(port)
-    #wait for 1 minimum client
-    server_Socket.listen(1)
-    #create connection socket
-    connection_socket, addr = server_Socket.accept()
-    while 1:
-        data = server_Socket.recv(init_Chunk)
+    if (sys.argv[1] == '-s'):
+        port = int(sys.argv[2])
+        if((port < 1024) or (port > 65535)):
+            print("Error: port number must be in the range 1024 to 65535")
+            exit()
+        data_sum = 0
+        hostconnect = input()
+        init_Chunk = bytearray(1000)
+        #create server socket
+        server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #bind port with socket
+        server_Socket.bind((hostconnect, port))
+        #wait for 1 minimum client
+        server_Socket.listen(1)
+        #create connection socket
+        connection_socket, addr = server_Socket.accept()
+        with connection_socket:
+            start_time = time.time()
+            while True:
+                data = server_Socket.recv(init_Chunk)
+                data_sum += 1
+                if not data:
+                    break
+            stop = time.time()
+            total_time = start_time - stop
+            rate = (data_sum / 125) / total_time
+            rate = float("%0.3f" % (rate))
+            print("received=" + str(data_sum) + " KB rate=" + str(rate) + " Mbps")
 
-    connection_socket.close()
+            connection_socket.close()
+    else:
+        print("Error: missing or additional arguments")
+        sys.exit()
     
     
 
-
-elif (len(sys.argv) == 4): 
+#client part
+if (len(sys.argv) == 4):
+    if (sys.argv[1] == '-s'):
+        print("Error: missing or additional arguments")
+        sys.exit()
 
     server_Hostname = str(sys.argv[1])
     server_Port = int(sys.argv[2])
@@ -40,8 +64,7 @@ elif (len(sys.argv) == 4):
     #-------------------------------------3 ARGS CREATED AND CHECKED->>>>>---SEND DATA, CALCUALTE, RETURN DATA+BANDWIDTH-----------------------------------------------------
 
     #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    address = (server_Hostname, server_Port)
-    #           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
+    address = (server_Hostname, server_Port)#           connection will be     IPV4(INET)         TCP(STREAM)                       -> IPV6(INET6)  UDP(DGRAM)
     client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_Socket.connect(address)
     init_Chunk = bytearray(1000)
@@ -57,10 +80,10 @@ elif (len(sys.argv) == 4):
 
     print("sent={} KB rate={} Mbps".format(counter, send_Rate))
     client_Socket.close()
+
 else:
     print("Error: missing or additional arguments")
-    exit()   
-
+    sys.exit()
 
     
 
